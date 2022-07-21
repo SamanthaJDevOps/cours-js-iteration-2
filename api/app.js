59715af0a5673dc2e7434fd26929811d0d935857
Samantha <samantha.jalabert@le-campus-numerique.fr>
2022-07-21 12:11:49 +0200

    fin api

--- api/app.js
+++ api/app.js
@@ -150,7 +150,7 @@ function get_types_by_comm(comm){
  * ayant pour un format de données celui passé en paramètre.
  */
 function get_types_by_format(format){
-    //types().types.map(elem => elem[1].sensors.map(sens => console.log(sens)));
+
     if(types().types.map(elem => elem[1].sensors.filter(sensor => sensor === format).length).reduce((a,b) => a + b) === 0){
         return undefined;
     }else {
@@ -189,58 +189,37 @@ function filter_objects_by_comm(comm){
  */
 function filter_objects_by_data_type(data_type){
 
-    // console.log(data_type);
-    const objets = data.objects;
-    const types = data.types;
+    const data_formats = objects().objects.map(o => data.types[o.type].sensors.filter(sens => data.data_formats[sens].data_type === data_type)).filter((tab) => tab.length);
 
-    // console.log(data_formats);
-    // console.log("\n\n\n");
-    // console.log(types);
-
-    console.log("\n\n\n");
-
-    const data_formats = objets.map(o => data.types[o.type].sensors.filter(sens => data.data_formats[sens].data_type === data_type)).filter((tab) => tab.length);
-    console.log(data_formats);
-
-    //objets.map(({type}) => console.log(type));
-    // const r = data_formats.map((el) => data.objects.type[data.types.sensors.filter(el)]);
-    // console.log(r);
-    // for(el in types) {
-    //     types[el].sensors.map(console.log);
+    // if(data_formats.length != 0){
+    //     data_formats.map(o =>console.log(o[0]));
+    //     const type_concern = [];
+    //     data_formats.forEach( o => {
+    //         const t = get_types_by_format(o[0]);
+    //         console.log('\n\n\n');
+    //         console.log(t.types[0][1].sensors);
+    //         console.log('\n\n\n');
+    //         type_concern.push(t.types);
+    //
+    //         type_concern.forEach(a => {
+    //             console.log('\n\n\n');
+    //             console.log(a[0][0]);
+    //             const obj = [];
+    //             obj.push(objects().objects.filter( i => i.type === a[0][0]));
+    //             console.log(obj);
+    //         });
+    //     });
+    //
     // }
     //
-    // console.log(data_types);
 
-    // types.map(console.log);
-    // if(data.data_formats.indexOf(data_type) < 0){
-    //     return undefined;
-    // }else{
-    //    const r =  data.objects.filter(o => data.types[o.type].communication == comm);
-    //     console.log(r);
-    //     return {objects : r};
-    // }
-    //data.data_formats.forEach(console.log);
-    //data.Object.entries(data_formats).forEach(element => console.log(element));
+    if(data_formats.length != 0){
+        const obj = data_formats.map(o => objects().objects.filter( i => i.type === get_types_by_format(o[0]).types[0][0]) );
+        obj.map(y =>  y['sensors'] = data.types[y[0].type].sensors );
 
-    // console.log("1 : ");
-    //
-    // //Object.entries(data.types).map(console.log);
-    // const r = Object.entries(data.types)[0];
-    // console.log(r);
-    //
-    // console.log("2 : ");
-    // Object.entries(data.types).forEach(element => console.log(element));
-
-    // console.log("3 : ");
-    // const entries = Object.entries((data.types));
-    // console.log("entries : ", entries);
-    //
-    //
-    // const map_entries = entries.map(console.log);
-    // const foreach = map_entries.forEach(element => element.filter(el => typeof(el) === object).map(console.log));
-
-    //return data.data_formats.indexOf(comm) < 0 ? undefined :
-    //Object.entries(data.types).map(o => o.
+        return {"objects" : obj};
+    }
+    return undefined;
 
 }
 
@@ -253,7 +232,25 @@ function filter_objects_by_data_type(data_type){
  * Les types de données des sensors et autres informations seront regroupé dans la clé sensors de l'objet.
  */
 function get_full_object_by_serial(serial){
-    return serial;
+
+    const obj = get_object_by_serial(serial);
+
+
+    if(obj === undefined){
+        return undefined;
+    }else{
+
+        obj.communication = data.types[obj.type].communication;
+        obj.default_image = data.types[obj.type].default_image;
+        let sensortype = data.types[obj.type].sensors;
+        let boite = [];
+        sensortype.map(sensor => {
+            boite[sensor] = data.data_formats[sensor];
+        });
+        obj.sensors = boite;
+
+        return obj;
+    }
 }
 
 /**
